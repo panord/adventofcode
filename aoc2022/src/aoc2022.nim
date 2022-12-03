03cd6d48d3c889a8dcc02cfcfca304dd0fff7b6d
Patrik Nordvall <patrik.nordvall95@gmail.com>
2022-12-03 22:06:10 +0100

    Add solution for day 2
    Signed-off-by: Patrik Nordvall <patrik.nordvall95@gmail.com>

--- aoc2022/src/aoc2022.nim
+++ aoc2022/src/aoc2022.nim
@@ -4,6 +4,7 @@
 import std/rdstdin
 import std/enumerate
 from std/strutils import parseUInt, splitLines
+from std/sequtils import filter
 
 
 
@@ -18,6 +19,121 @@ proc max[T: SomeInteger](list: seq[T]): (int, T) =
 
         return (i, m)
 
+
+proc rps_win(cand: char, opp: char): int =
+        case opp:
+        of 'A':
+                case cand:
+                of 'X':
+                        return 0
+                of 'Y':
+                        return 1
+                of 'Z':
+                        return -1
+                else:
+                        discard
+        of 'B':
+                case cand:
+                of 'X':
+                        return -1
+                of 'Y':
+                        return 0
+                of 'Z':
+                        return 1
+                else:
+                        discard
+
+        of 'C':
+                case cand:
+                of 'X':
+                        return 1
+                of 'Y':
+                        return -1
+                of 'Z':
+                        return 0
+                else:
+                        discard
+        else:
+                discard
+
+        raise newException(ValueError, "Invalid rock paper scissor move")
+
+proc calc_score[T: SomeInteger](cand: char, opp: char, score: var T): void =
+                case rps_win(cand, opp):
+                of 1:
+                        score += 6
+                of 0:
+                        score += 3
+                of -1:
+                        score += 0
+                else:
+                        raise newException(ValueError, "Invalid rock paper scissor move")
+
+                case cand:
+                of 'X':
+                        score += 1
+                of 'Y':
+                        score += 2
+                of 'Z':
+                        score += 3
+                else:
+                        raise newException(ValueError, "Invalid rock paper scissor move")
+
+proc calc_choice[T: SomeInteger](cand: char, opp: char, score: var T): void =
+        case cand:
+        of 'X':
+                score += 0
+                case opp
+                of 'A':
+                        score += 3
+                of 'B':
+                        score += 1
+                of 'C':
+                        score += 2
+                else:
+                        raise newException(ValueError, "Invalid rock paper scissor move")
+        of 'Y':
+                score += 3
+                case opp:
+                of 'A':
+                        score += 1
+                of 'B':
+                        score += 2
+                of 'C':
+                        score += 3
+                else:
+                        raise newException(ValueError, "Invalid rock paper scissor move")
+
+        of 'Z':
+                score += 6
+                case opp:
+                of 'A':
+                        score += 2
+                of 'B':
+                        score += 3
+                of 'C':
+                        score += 1
+                else:
+                        raise newException(ValueError, "Invalid rock paper scissor move")
+
+
+        else:
+                raise newException(ValueError, "Invalid rock paper scissor outcome")
+
+
+proc taskTwo(part: Natural): void =
+        let input = splitLines(readAll(stdin))
+        var score: uint64 = 0;
+        for l in filter(input, proc(x: string): bool = x != ""):
+                case part:
+                of 1:
+                        calc_score(l[2], l[0], score)
+                of 2:
+                        calc_choice(l[2], l[0], score)
+                else:
+                        raise newException(ValueError, "Invalid task part")
+
+        echo "Total score: ", score
 
 proc taskOne(): void =
         var list: seq[uint64]
@@ -45,15 +161,27 @@ proc taskOne(): void =
 
         echo "Total: ", tot
 
-when isMainModule:
+
+proc main(): int =
         echo("Hello, World!")
         var taskStr: string
-        var ok = readLineFromStdin("Which task? ", taskStr)
+        var partStr: string
+
+        if not readLineFromStdin("Which task? ", taskStr):
+                echo "Failed to read task"
+                return -1
+
+        if not readLineFromStdin("Which task part? ", partStr):
+                echo "Failed to read task part"
+                return -1
 
         case parseUInt(taskStr)
         of 1:
                 taskOne()
+        of 2:
+                taskTwo(parseUInt(partStr))
         else:
                 echo("Not implemented!")
 
-
+when isMainModule:
+        discard main()
